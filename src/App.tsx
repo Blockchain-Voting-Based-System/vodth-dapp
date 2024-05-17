1e747933787154a7fea5e3ee56080b12491be475
vaneath <vaneathzz@gmail.com>
2024-05-17 13:22:26 +0700

    close #3 setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { WalletStatus } from "./WalletStatus";
 function App() {
   return (
     <>
+      <h1 className="text-red-500">Vaneath</h1>
       <Flex
         position="sticky"
         px="4"

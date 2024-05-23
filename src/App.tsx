2b2e330f461fd1ec031c7266f24791d462d46080
Moyuraa <vannthourayin@gmail.com>
2024-05-23 12:00:43 +0700

    close #9 add dashbaord ui for dapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,49 @@
-import { ConnectButton } from "@mysten/dapp-kit";
-import { Box, Container, Flex, Heading } from "@radix-ui/themes";
-import { WalletStatus } from "./WalletStatus";
+import React, { useState, useEffect } from 'react';
+// import { ConnectButton } from "@mysten/dapp-kit";
+// import { Box, Container, Flex, Heading } from "@radix-ui/themes";
+// import { WalletStatus } from "./WalletStatus";
+import Sidebar from './components/sidebar/Sidebar';
+import Main from './components/main/Main';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [darkMode]);
+
   return (
     <>
-      <h1 className="text-red-500">Vaneath</h1>
-      <Flex
-        position="sticky"
-        px="4"
-        py="2"
-        justify="between"
-        style={{
-          borderBottom: "1px solid var(--gray-a2)",
-        }}
-      >
-        <Box>
-          <Heading>dApp Starter Template</Heading>
-        </Box>
-
-        <Box>
-          <ConnectButton />
-        </Box>
-      </Flex>
-      <Container>
+      <div className="flex h-screen bg-slate-900 dark:bg-gray-900">
+        <section className="w-[10%] sm:w-[15%]">
+          <Sidebar />
+        </section>
+        <section className="flex flex-col w-[90%] sm:w-[85%] overflow-auto">
+          <Main />
+        </section>
+      </div>
+      {/* <div className="fixed top-0 left-0 right-0 bg-white dark:bg-gray-800 shadow-md z-10">
+        <Flex
+          px="4"
+          py="2"
+          justify="between"
+          style={{
+            borderBottom: "1px solid var(--gray-a2)",
+          }}
+        >
+          <Box>
+            <Heading className="text-black dark:text-white">dApp Starter Template</Heading>
+          </Box>
+          <Box>
+            <ConnectButton />
+          </Box>
+        </Flex>
+      </div>
+      <Container className="mt-16"> 
         <Container
           mt="5"
           pt="2"
@@ -32,7 +52,13 @@ function App() {
         >
           <WalletStatus />
         </Container>
-      </Container>
+      </Container> */}
+       <button //darkmode button
+        className="fixed bottom-4 right-4 bg-blue-500 text-white p-2 rounded"
+        onClick={() => setDarkMode(!darkMode)}
+      >
+        Dark Mode
+      </button> 
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import { Theme } from "@radix-ui/themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 
+
 const queryClient = new QueryClient();
 
 const { networkConfig } = createNetworkConfig({

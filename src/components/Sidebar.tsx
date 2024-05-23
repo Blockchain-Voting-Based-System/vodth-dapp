738849b7744708163818d7f23267eeba03bd8f07
Moyuraa <vannthourayin@gmail.com>
2024-05-23 12:11:01 +0700

    close #9 add dashbaord ui for dapp

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,71 @@
+import { AiOutlineDashboard } from "react-icons/ai";
+import { FaChartArea } from "react-icons/fa";
+import { HiUserGroup } from "react-icons/hi2";
+import { ImUserTie } from "react-icons/im";
+import { IoSettingsOutline } from "react-icons/io5";
+import { LuUserCircle2 } from "react-icons/lu";
+
+const Sidebar = () => {
+  return (
+    <div className="h-screen dark:bg-slate-950">
+      <div className="flex flex-col gap-3 w-full text-slate-300 h-full justify-between">
+        <div className="flex flex-col gap-10 px-4 mt-4">
+          <div className="flex items-center justify-center gap-1"></div>
+          <span className="text-sm font-bold sm:text-sm text-blue-100 text-center">
+            Welcome to, Vodth!
+          </span>
+          <div className="flex flex-col gap-5 text-md sm:text-sm lg:text-lg">
+            <div className="flex items-center gap-2">
+              <div>
+                <AiOutlineDashboard></AiOutlineDashboard>
+              </div>
+              <div className="hidden sm:flex hover:text-slate-100 cursor-pointer">
+                Dashboard
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <div>
+                <LuUserCircle2 />
+              </div>
+              <div className="hidden sm:flex hover:text-slate-100 cursor-pointer">
+                Voters
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <div>
+                <ImUserTie />
+              </div>
+              <div className="hidden sm:flex hover:text-slate-100 cursor-pointer">
+                Candidates
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <div>
+                <HiUserGroup />
+              </div>
+              <div className="hidden sm:flex hover:text-slate-100 cursor-pointer">
+                Parties
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <div>
+                <FaChartArea />
+              </div>
+              <div className="hidden sm:flex hover:text-slate-100 cursor-pointer">
+                Election Areas
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center text-md sm:text-xs md:text-sm lg:text-lg px-4 mb-4 gap-2">
+          <div>
+            <IoSettingsOutline />
+          </div>
+          <div className="hidden sm:flex">Setting</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;

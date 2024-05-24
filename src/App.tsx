a9ceb8233e9b93a54a61573cd0fc4124c6aa20ab
Vuthy Outhdom <vuthyouthdom01@gmail.com>
2024-05-24 15:09:12 +0700

    close #11 integrate zk login

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import {
 import DefaultLayout from "./layouts/DefaultLayout";
 import EventPage from "./pages/EventPage";
 import HomePage from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<DefaultLayout />}>
       <Route index element={<HomePage />} />
       <Route path="events" element={<EventPage />} />
+      <Route path="login" element={<LoginPage />} />
     </Route>,
   ),
 );
